921392182bccbcddd5e34a40a4ca4392de652782
JACK <109014373+JACKSAMAA@users.noreply.github.com>
2022-08-04 10:52:38 +0530

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -93,7 +93,7 @@ PM_START_TEXT = """
 
 buttons = [
     [
-        InlineKeyboardButton(text=f"Owner State , url=f"t.me/Mysticpfp")
+        InlineKeyboardButton(text=f"Owner State , url=f"t.me/Mysticpfp"), 
     ],
     [
         InlineKeyboardButton(text="Get Help", callback_data="help_back"),
